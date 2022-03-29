39a1ad98871ee506b42792c9b628fbf4a3c0e537
rajashekhar balya <96037024+rajashekharbalya111@users.noreply.github.com>
2022-03-29 10:24:48 +0530

    Delete prodbackend.tf

--- prodbackend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "AzureRm" {
-                       resource_group_name  =  "shekarcloud"
-                       storage_account_name =  "shekarcloudstorage"
-                       container_name       =  "storage"
-                       key                  =  "prod.terraform.tfstate"
-
-                     }
-          }
